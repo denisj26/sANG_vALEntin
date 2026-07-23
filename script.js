cc7db5b6abfc7a86516b43f59dcef45a95a8606d
denisj26 <denisjairp@gmail.com>
2026-07-22 23:08:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Detectar selección de texto en la página
 document.addEventListener("selectionchange", () => {
   const selection = window.getSelection().toString();
   const egg = document.getElementById("easterEgg");
 
-  if (selection.length > 5) {
+  if (selection.length > 10) {
     egg.style.opacity = "1";
   } else {
     egg.style.opacity = "0";
