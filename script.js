efa416c04249705d3f2afa0842058f394c7a2be5
denisj26 <denisjairp@gmail.com>
2026-07-22 22:55:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Cambia el título dinámicamente (más realista)
+// Título dinámico tipo página real
 document.title = "404 - Página no encontrada";
 
-// Simulación de error en consola (detalle pro 😎)
+// Mensaje en consola (detalle pro)
 console.error("404 Error: Página no encontrada");
