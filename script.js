71e555e320125f6cada214b20fe8fafa97b809df
denisj26 <denisjairp@gmail.com>
2026-07-22 23:11:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.addEventListener("selectionchange", () => {
-  const selection = window.getSelection().toString();
+// Detectar cuando el usuario selecciona texto
+document.addEventListener("mouseup", () => {
+  const selectedText = window.getSelection().toString();
+
   const egg = document.getElementById("easterEgg");
 
-  if (selection.length > 10) {
+  if (selectedText.includes("404")) {
     egg.style.opacity = "1";
   } else {
     egg.style.opacity = "0";
   }
+});
+
+// Opcional: ocultar si hacen click fuera
+document.addEventListener("click", () => {
+  const egg = document.getElementById("easterEgg");
+  egg.style.opacity = "0";
 });
